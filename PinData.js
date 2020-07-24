93a986ae5a53ac7b6f9285c6effba4f34550d4d0
Gregor Suttie <gsuttie2001@hotmail.com>
2020-07-24 15:33:03 +0100

    Add files via upload

---
+++ PinData.js
@@ -0,0 +1,23 @@
+﻿var vPinData = {
+    "Pins": [
+        {
+            "deviceid": "0",
+            "name": "Joe Bloggs",
+            "long": "-0.133940",
+            "lat": "50.957480",
+            "text": "@gregor_suttie | https://gregorsuttie.com",
+            "status": "",
+            "image": ""
+        },
+        {
+            "deviceid": "1",
+            "name": "Greg Suttie",
+            "long": "-0.133940",
+            "lat": "53.957480",
+            "text": "@suttieg | suttieg.com",
+            "status": "",
+            "image": ""
+        }
+    ]
+};
+
